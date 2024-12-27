736179c8c78264779f6c715d03bb6b9a199759cf
ImShubham09 <singhashubham612@gmail.com>
2024-12-27 21:01:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let timerdisplay = document.getElementById("timer");
+
+let currDate = new Date();
+
+setInterval{
+	function(){
+		currDate = new Date();
+		timerdisplay.innerHTML = currDate.toLocaleString();
+	},
+	1000
+}
